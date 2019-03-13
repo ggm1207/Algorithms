a75112ea835f2b3318026cc78b16273060b2d982
Gunmo <ticonhi@naver.com>
2019-03-13 11:56:34 +0900

    cctv

--- 1.baekjoon/B (31 ~ 60)/S_15683_37%.py
+++ 1.baekjoon/B (31 ~ 60)/S_15683_37%.py
@@ -1,37 +1,34 @@
 import sys
+import copy
 
 sys.setrecursionlimit(100000)
-
+sys.getrecursionlimit()
 cctv_list = ['1','2','3','4','5','#']
 
 class CCTV():
-    def __init__(self,n,m):
+    def __init__(self,n,m, arr):
         self.n = n
         self.m = m
-        self.arr = self.make_arr()
-        # self.check_arr()
+        self.arr = self.make_arr(arr)
+        # self.check_arr(self.arr)
         self.position = self.extract_position()
-        self.allof = []
+        self.allof = [] # 모든 경우의 수를 저장
         self.solve_cctv()
         # self.check_arr()
+        # self.allof = list(set(self.allof))
+        # print(self.allof)
         print(min(self.allof))
 
-    def make_arr(self):
+    def make_arr(self, ori_arr):
         arr = []
-        for i in range(self.n+2):
-            arr.append(['6' for i in range(self.m+2)])
-        ori = []
+        for _ in range(self.n+2):
+            arr.append(['6' for _ in range(self.m+2)])
+        
         for i in range(self.n):
-            ori.append(input().split(' '))
-        i, j = 1,1
-        for ri in ori:
-            j = 1
-            for r in ri:
-                arr[i][j] = r
-                j += 1
-            i += 1
-        return arr  
+            arr[i+1][1:self.m+1] = ori_arr[i][:]
     
+        return arr
+
     def check_arr(self,arr):
         for array in arr:
             print(array)
@@ -45,12 +42,10 @@ class CCTV():
                     continue
                 else:
                     position.append((self.arr[i][j],(i,j)))
-        position = sorted(position)[::-1]
-        print('pos: ', position)
+        # print('pos: ', position)
         return position
 
     def solve_cctv(self):
-        max_num = 0
         position = []
         for cctvnum, (x,y) in self.position:
             if cctvnum == '5':
@@ -60,19 +55,26 @@ class CCTV():
                 self.arrow_fill(self.arr,'down',x,y)
             else:
                 position.append((cctvnum,(x,y)))
-        self.position = position
         self.maxcount = len(position)
-        self.check_arr(self.arr)
+        self.position = position
+        # print(self.maxcount)
+        # self.check_arr(self.arr)
         self.rotate(self.arr, 1)
-    
-    def rotate(self, arr, count):
-        if count == self.maxcount:
+
+    def rotate(self, arr, count): # 5번 cctv를 처리하고 온 배열
+        # print('count: ', count)
+        # print('maxcount: ', self.maxcount)
+        # print(count == (self.maxcount + 1))
+        if count == (self.maxcount + 1):
             # self.check_arr(arr)
             self.allof.append(self.solve_arr(arr))
             return 
         
         cctvnum, (x,y) = self.position[count - 1]
-        ori_arr = arr
+        # if x == 1:
+        #     print('x는 1이다')
+        # print('info:' ,cctvnum, x, y)
+        ori_arr = copy.deepcopy(arr)
         if cctvnum == '4':
             positive = [
                 ['left','up','right'],
@@ -80,12 +82,12 @@ class CCTV():
                 ['right','down','left'],
                 ['down','left','up']]
             for posit in positive:
-                self.arrow_fill(ori_arr,posit[0],x,y)
-                self.arrow_fill(ori_arr,posit[1],x,y)
-                self.arrow_fill(ori_arr,posit[2],x,y)
-                self.rotate(ori_arr, count+1)
+                arrs = copy.deepcopy(ori_arr)
+                self.arrow_fill(arrs,posit[0],x,y)
+                self.arrow_fill(arrs,posit[1],x,y)
+                self.arrow_fill(arrs,posit[2],x,y)
+                self.rotate(arrs, count+1)
                 
-            
         elif cctvnum == '3':
             positive = [
                 ['left','up'],
@@ -93,25 +95,28 @@ class CCTV():
                 ['right','down'],
                 ['down','left']]
             for posit in positive:
-                self.arrow_fill(ori_arr,posit[0],x,y)
-                self.arrow_fill(ori_arr,posit[1],x,y)
-                self.rotate(ori_arr, count+1)
+                arrs = copy.deepcopy(ori_arr)
+                self.arrow_fill(arrs,posit[0],x,y)
+                self.arrow_fill(arrs,posit[1],x,y)
+                self.rotate(arrs, count+1)
             
         elif cctvnum == '2':
             positive = [
                 ['left','right'],
                 ['up','down']]
             for posit in positive:
-                self.arrow_fill(ori_arr,posit[0],x,y)
-                self.arrow_fill(ori_arr,posit[1],x,y)
-                self.rotate(ori_arr, count+1)
+                arrs = copy.deepcopy(ori_arr)
+                self.arrow_fill(arrs,posit[0],x,y)
+                self.arrow_fill(arrs,posit[1],x,y)
+                self.rotate(arrs, count+1)
                 
-            
-        elif cctvnum == '1':
+        else:
+            # print('time to 1!')
             positive = ['left','up','right','down']
             for posit in positive:
-                self.arrow_fill(ori_arr,posit,x,y)
-                self.rotate(ori_arr, count+1)
+                arrs = copy.deepcopy(ori_arr)
+                self.arrow_fill(arrs,posit,x,y)
+                self.rotate(arrs, count+1)
                 
         
     def solve_arr(self,arr):
@@ -121,8 +126,6 @@ class CCTV():
         return counts
         
         
-
-
 
     # def arrow_find(self, direction, i, j):
     #     if direction == 'left':
@@ -160,6 +163,10 @@ class CCTV():
             return self.arrow_fill(arr, direction, x, y)
 
 if __name__ == "__main__":
-    n, m = list(map(int ,input().split()))
-    CCTV(n,m)
+    n, m = list(map(int ,sys.stdin.readline().split()))
+    arr = []
+    for i in range(n):
+        arr.append(sys.stdin.readline().split())
+    
+    CCTV(n,m,arr)
     
