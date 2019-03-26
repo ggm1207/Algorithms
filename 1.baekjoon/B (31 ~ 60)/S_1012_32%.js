3caf82d00e8965ffd680aeb07797a49d8bab4d90
Gunmo <ticonhi@naver.com>
2019-03-26 13:32:10 +0900

    Update S_1012_32%.js

--- 1.baekjoon/B (31 ~ 60)/S_1012_32%.js
+++ 1.baekjoon/B (31 ~ 60)/S_1012_32%.js
@@ -6,8 +6,6 @@ class bugpower{
         this.n = n;
         this.m = m;
         this.count = 0;
-        this.check();
-        console.log(this.count);
     }
 
     check(){
@@ -15,26 +13,33 @@ class bugpower{
         for(i=0; i<this.n; i++){
             for(j=0; j<this.m; j++){
                 if(this.arr[i][j]===1){
-                    console.log(i, j);
-                    this.ext(i,j);
+                    // console.log(i, j);
+                    var a = setTimeout(this.ext(i,j),1000);
                 }
             }
         }
+        return this.count
     }
-    ext(i,j){
+    async ext(i,j){
         if(i >= this.n || i < 0 || j >= this.m || j < 0){
             return;
         }
         if(this.arr[i][j]==1){
+            console.log(i,j)
             this.arr[i][j] = 0;
             this.count = this.count + 1;
-            this.ext(i+1,j);
-            this.ext(i-1,j);
-            this.ext(i,j+1);
-            this.ext(i,j-1);
+            await this.ext(i+1,j);
+            await this.ext(i-1,j);
+            await this.ext(i,j+1);
+            await this.ext(i,j-1);
         } else {
             return;
         }
+    }
+
+    async show(){
+        await this.check()
+        console.log(this.count)
     }
 }
 
@@ -58,5 +63,7 @@ arr[0][1] = 1;
 arr[2][2] = 1;
 arr[3][3] = 1;
 console.log(arr);
-console.log(arr[1][3]);
+// console.log(arr[1][3]);
 let a = new bugpower(arr,4,5);
+// a.check()
+a.show()
