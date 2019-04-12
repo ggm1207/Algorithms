93d3d4f97fc11af80f47d5c386ded222a1de731c
ggm1207 <40788624+ggm1207@users.noreply.github.com>
2019-04-12 15:56:26 +0900

    Update US_15684_20%.py

--- 1.baekjoon/A (0 ~ 30)/US_15684_20%.py
+++ 1.baekjoon/A (0 ~ 30)/US_15684_20%.py
@@ -1,21 +1,70 @@
+MIS = lambda: map(int,input().split())
+
 import numpy as np
 
 # def main():
 
+# def fill_x(ladder,a,b):
+#     ladder[b-1][a-1] = 'l'
+#     if b-2 >= 0:
+#         ladder[b-2][a-1] = 'x'
+#     if b < N-1:
+#         ladder[b][a-1] = 'x'
+
+
+# go and back possible count + 1 and add
+# go and back not possible count + 1 and go
+
+def check(line):
+    count = 0
+    for li_num in range(1,N+1):
+        now_li = li_num
+        for li_now in range(1,H+1):
+            if (now_li, li_row) in line:
+                if checking_line(now_li, li_row): # high empty 1 low empty 2
+                    now_li = li_row
+                else:
+                    now_li = li_row + 1 # change line
+                
+                count += 1
+                continue
+            if (now_li - 1, li_row) in line:
+                if checking_line(now_li, li_row):
+                    now_li = li_now + 1
+                else:
+                    now_li = li_now
+
+                count += 1
+                continue
+            
+def checking_line(line,a):
+    if ()
+
 
 
 if __name__ == "__main__":
-    # 첫째 줄에 세로선의 개수 N, 가로선의 개수 M, 
-    # 세로선마다 가로선을 놓을 수 있는 위치의 개수 H가 주어진다. 
-    N, M, H = list(map(int, input().split()))
-    width = []
-    for i in range(M):
-        width.append(tuple(map(int, input().split())))
-    # 가로선의 정보는 두 정수 a과 b로 나타낸다. 
-    # (1 ≤ a ≤ H, 1 ≤ b ≤ N-1) b번 세로선과 b+1번 세로선을 
-    # a번 점선 위치에서 연결했다는 의미이다.
-    
-    # for w in width:
-    #     print(w)
+    N, M, H = MIS()
+    ladder = []
+    line = {}
 
-    
+    # for i in range(N-1):
+    #     ladder.append(['0' for _ in range(H)])
+
+    # for _ in range(int(M)):
+    #     a, b = MIS()
+    #     fill_x(ladder, a, b)
+
+    for _ in range(int(M)):
+        a, b = MIS()
+        line[(a,b)] = b+1
+
+    print(line)
+    for l in line:
+        print(l)
+
+
+
+
+
+# (possible number, line to be modified)
+#    
