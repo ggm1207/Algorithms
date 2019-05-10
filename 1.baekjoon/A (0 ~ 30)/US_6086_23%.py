ac446f0e513f883249c333b86925d2fcf6a23b3d
Gunmo <ticonhi@naver.com>
2019-05-10 17:23:42 +0900

    breef

---
+++ 1.baekjoon/A (0 ~ 30)/US_6086_23%.py
@@ -0,0 +1,18 @@
+import sys
+import string
+
+MIS = lambda : sys.stdin.readline().split()
+
+def ctoi(c):
+    if c <= 'Z':
+        return c - 'A'
+    return c - 'A'
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline())
+    c = []
+    f = []
+    for i in range(N):
+        u, v, w = MIS()
+    
+    print(vertex)
