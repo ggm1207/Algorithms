1d2c894b1190af9b9f07d5819cf212259b819679
ggm1207 <40788624+ggm1207@users.noreply.github.com>
2019-05-16 14:48:29 +0900

    mate

---
+++ 1.baekjoon/B (31 ~ 60)/US_1007_36%.py
@@ -0,0 +1,13 @@
+import sys
+MIS = map(int,sys.stdin.readline())
+
+def main():
+    N_pos =  int(sys.stdin.readline())
+    M_position = [MIS() for _ in range(N_pos)]
+
+
+
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        main()
