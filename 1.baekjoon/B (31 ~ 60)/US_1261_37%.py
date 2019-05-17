847c1170cd2adbfc92bc8947a27e35cadd3ea31b
Gunmo <ticonhi@naver.com>
2019-05-17 23:16:23 +0900

    Update US_1261_37%.py

--- 1.baekjoon/B (31 ~ 60)/US_1261_37%.py
+++ 1.baekjoon/B (31 ~ 60)/US_1261_37%.py
@@ -3,47 +3,71 @@ import collections
 
 MIS = lambda : sys.stdin.readline()[:-1]
 
+def show():
+    for ar in visited:
+        print(ar)
 def fake():
     q = collections.deque()
     q.append((0,0,0))
     crushnum = []
+
     while(1):
         if not len(q):
             break
-        y,x,cnt = q.popleft()
-        print( y, x, cnt)
-        if cnt > n + m:
-            break
-        
-        if visited[y][x]:
-            if cnt > min(visited[y][x]):
-                continue
 
-        visited[y][x].append(cnt)
+        y,x,cnt = q.popleft()
+        # print(y, x, cnt)
+        # show()
+
         if y == m-1 and x == n-1:
             crushnum.append(cnt)
             continue
 
-        if (0 <= y+1 < m) and (0 <= x < n):
-            if arr[y+1][x] == '0':
-                q.append((y+1,x,cnt))
+        if 0 <= y+1 < m and 0 <= x < n:
+            if cnt >= visited[y+1][x]:
+                pass
             else:
-                q.append((y+1,x,cnt+1))
-        if (0 <= y-1 < m) and (0 <= x < n):
-            if arr[y-1][x] == '0':
-                q.append((y-1,x,cnt))
+                if arr[y+1][x] == '0':
+                    q.append((y+1,x,cnt))
+                    visited[y+1][x] = cnt
+                else:
+                    if cnt+1 < visited[y+1][x]:
+                        q.append((y+1,x,cnt+1))
+                        visited[y+1][x] = cnt+1
+        if 0 <= y-1 < m and 0 <= x < n:
+            if cnt >= visited[y-1][x]:
+                pass
             else:
-                q.append((y-1,x,cnt+1))
-        if (0 <= y < m) and (0 <= x+1 < n):
-            if arr[y][x+1] == '0':
-                q.append((y,x+1,cnt))
+                if arr[y-1][x] == '0':
+                    q.append((y-1,x,cnt))
+                    visited[y-1][x] = cnt
+                else:
+                    if cnt+1 < visited[y-1][x]:
+                        q.append((y-1,x,cnt+1))
+                        visited[y-1][x] = cnt+1
+        if 0 <= y < m and 0 <= x+1 < n:
+            if cnt >= visited[y][x+1]:
+                pass
             else:
-                q.append((y,x+1,cnt+1))
-        if (0 <= y < m) and (0 <= x-1 < n):
-            if arr[y][x-1] == '0':
-                q.append((y,x-1,cnt))
+                if arr[y][x+1] == '0':
+                    q.append((y,x+1,cnt))
+                    visited[y][x+1] = cnt
+                else:
+                    if cnt+1 < visited[y][x+1]:
+                        q.append((y,x+1,cnt+1))
+                        visited[y][x+1] = cnt
+        if 0 <= y < m and 0 <= x-1 < n:
+            if cnt >= visited[y][x-1]:
+                pass
             else:
-                q.append((y,x-1,cnt+1))
+                if arr[y][x-1] == '0':
+                    q.append((y,x-1,cnt))
+                    visited[y][x-1] = cnt
+                else:
+                    if cnt+1 < visited[y][x-1]:
+                        q.append((y,x-1,cnt+1))
+                        visited[y][x-1] = cnt
+    # print(crushnum)
     print(min(crushnum))
 # 0, 0 -> N-1,M-1
 # '0' : empty room
@@ -51,5 +75,5 @@ def fake():
 if __name__ == "__main__":
     n,m = map(int,sys.stdin.readline().split())
     arr = [list(MIS()) for _ in range(m)]
-    visited = [[[] for _ in range(n)] for _ in range(m)]
+    visited = [[100000000000 for _ in range(n)] for _ in range(m)]
     fake()
