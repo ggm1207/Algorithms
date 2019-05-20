80ca36f822e0ff3995db9738eb502e202d6c4319
Gunmo <ticonhi@naver.com>
2019-05-20 10:11:14 +0900

    g

--- 1.baekjoon/B (31 ~ 60)/US_1007_36%.py
+++ 1.baekjoon/B (31 ~ 60)/US_1007_36%.py
@@ -1,9 +1,12 @@
 import sys
-MIS = map(int,sys.stdin.readline())
+MIS = lambda : tuple(map(int,sys.stdin.readline().split()))
 
 def main():
     N_pos =  int(sys.stdin.readline())
     M_position = [MIS() for _ in range(N_pos)]
+    print(M_position)
+
+# sqrt(a^2 + b^2)
 
 if __name__ == "__main__":
     T = int(input())
