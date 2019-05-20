cd66750d5d70be3ff5e73292fefb3f4fc9c2e0cf
ggm1207 <40788624+ggm1207@users.noreply.github.com>
2019-05-20 18:17:33 +0900

    Create US_2156_34%.py

---
+++ 1.baekjoon/B (31 ~ 60)/US_2156_34%.py
@@ -0,0 +1,19 @@
+import sys
+
+MIS = lambda : int(sys.stdin.readline())
+
+def greatAlc():
+    grapeValue = []
+    while(1):
+        
+        
+        
+
+
+if __name__ == '__main__':
+    n = MIS()
+    grapeAlc_list = [MIS() for _ in range(n)]
+    greatAlc()
+    
+    
+    
