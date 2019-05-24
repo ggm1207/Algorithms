e3030aa319716ac5b1a6872a63345624c1209698
Gunmo <ticonhi@naver.com>
2019-05-24 12:32:23 +0900

    Update min_path.py

--- 3.others/min_path.py
+++ 3.others/min_path.py
@@ -1,5 +1,9 @@
 from sys import stdin
 import collections
+import matplotlib.pyplot as plt
+import random
+from tqdm import tqdm
+
 MIS = lambda : list(map(int,stdin.readline().split()))
 
 q = collections.deque()
@@ -10,7 +14,14 @@ def fake():
     unselected = b_list[1:]
     arrow = []
     cur = b_list[0] # (1,1)
+    process = 0
     while(1):
+        if process % 100:
+            process += 1
+        else:
+            process += 1
+            print(len(unselected), end = '\r')
+            
         if not unselected:
             break
 
@@ -31,20 +42,38 @@ def fake():
         if nextcur == cur:
             unselected.remove(nextpos)
             selected.append(nextpos)
-            arrow.append(str(cur)+'->'+str(nextpos))
+            arrow.append((cur,nextpos))
         else:
             unselected.remove(nextpos)
             selected.append(nextpos)
-            arrow.append(str(nextcur)+'->'+str(nextpos))
+            arrow.append((nextcur,nextpos))
         cur = nextcur
-    print(arrow)
+
+    show(arrow)
 
 def getcost(a,b):
     return (a[0]-b[0])**2 + (a[1]-b[1])**2
 
+
+def show(listdata):
+    x_list = []
+    y_list = []
+
+    for cur, nex in listdata:
+        x1, y1 = cur
+        x2, y2 = nex
+        x_list.append((x1,x2))
+        y_list.append((y1,y2))
+
+    # plt.plot([3,4],[3,5])
+    #(3,3) -> (4,5)
+    for i in range(len(x_list)):
+        plt.plot(x_list[i],y_list[i])
+
+    plt.show()
+
 if __name__ == "__main__":
     N = int(stdin.readline())
-    b_list = [MIS() for i in range(N)]
-    print(b_list)
+    b_list = [[random.random(),random.random()] for i in tqdm(range(N))]
     fake()
     
