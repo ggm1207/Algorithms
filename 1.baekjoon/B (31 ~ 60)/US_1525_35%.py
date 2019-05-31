4470b7a20e7c8ee4a2e7d288fcbd0ac15dda57ab
Gunmo <ticonhi@naver.com>
2019-05-31 11:48:00 +0900

    Update US_1525_35%.py

--- 1.baekjoon/B (31 ~ 60)/US_1525_35%.py
+++ 1.baekjoon/B (31 ~ 60)/US_1525_35%.py
@@ -6,50 +6,69 @@ MIS = lambda : map(int, sys.stdin.readline().split())
 
 q = []
 nextPos = [(0,1),(0,-1),(1,0),(-1,0)]
-
-def fake():
-    x , y = nums[0]
-    heqppush(q, (0, nums[0]))
-    maxnum = 0
-    while(1):
-        depth, x,y = q.heappop()
-        nextlist = []
-        for a,b in nextPos:
-            if (x+a,y+b) in list(nums.values()):
-                nextlist.append((x+a,y+b))
+visited = {}
+# push -> var
+# q 에서 꺼내오고
+# 가능한 var 계산후 q push
+# 
+def fake(): # inp : int
+    flag = True
+    a = str(inp)[0]
+    heapq.heappush(q,(0 + getCount(str(inp)),inp))
+    i = 0
+    while(q):
+        count, posvalue = heapq.heappop(q)
+        # print( count, posvalue)
+        visited[posvalue] = count
         
-        for poss in nextlist: # 0 근처 위치
-            heqppush(depth ,poss)
-            
+        depth = count - getCount(str(posvalue))
+        if var1 == posvalue:
+            print(depth)
+            return
+        getPosValue(depth, posvalue)
+    print(-1)
+        # if i == 10:
+        #     break
+        # i += 1
 
-def check():
-    count = 0
-    for i in range(3):
-        for j in range(3):
-            if 3*i+j+1 < 9:    
-                if (i,j) == nums[3*i+j+1]:
-                    count += 1
-            else:
-                if (2,2) == nums[0]:
-                    count += 1
-    return count
 
+def getCount(posvalue): # posvalue : str
+    return sum(list(map(lambda x, y : x == y,var,posvalue)))
+    
+def getPosValue(depth, posvalue): # push next q
+    posvalue = list(str(posvalue))
+    index = posvalue.index('9')
+    nextList = []
+    a, b = numtopos(index)
+    for x, y in nextPos:
+        nextList.append(postonum((a + x,b + y)))
+    nextList = list(filter(lambda x : 0 <= x < 9, nextList))
+    for nextIndex in nextList:
+        tmp = copy.copy(posvalue)
+        tmp[index], tmp[nextIndex] = tmp[nextIndex], tmp[index]
+        n = (depth + 1 + getCount(tmp), int("".join(tmp)))
+        dddd = n[1]
+        # print(dddd)
+        if dddd in visited:
+            if visited[dddd] > n[0]:
+                heapq.heappush(q,n)
+        else:
+            visited[dddd] = n[0]
+            heapq.heappush(q,n)
+    
+def numtopos(num):
+    return divmod(num-1,3)
+    
+def postonum(pos):
+    return pos[0]*3 + pos[1] + 1
 
 
 if __name__ == "__main__":
-    var = [[1,2,3],[4,5,6],[7,8,0]]
-    nums = {}
-    pos = {}
+    var1 = 123456789
+    var = '123456789'
+    inp = []
     for i in range(3):
-        a, b, c = MIS()
-        nums[a] = (i,0)
-        nums[b] = (i,1)
-        nums[c] = (i,2)
-        pos[(i,0)] = a
-        pos[(i,1)] = b
-        pos[(i,2)] = c
-
-    print(nums)
-    print(pos)
+        inp += sys.stdin.readline().split()
+    inp[inp.index('0')] = '9'
+    inp = int("".join(inp))
     fake()
-    print(check())
