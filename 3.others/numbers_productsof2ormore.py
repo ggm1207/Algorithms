63b59b4102bf698ea2c6da92b3686d8756e88eb9
gunmo <ggm1207@github.com>
2019-09-24 09:47:36 +0900

    느낀점 추가

--- 3.others/numbers_productsof2ormore.py
+++ 3.others/numbers_productsof2ormore.py
@@ -1,3 +1,9 @@
+"""느낀점
+origin, insertion, bin_search 중에서 백만이 입력으로 주어졌을 때는 insertion이 가장 빨랐다.
+value_list의 길이가 생각보다 작아서 bin_search 가 힘을 못 썼을 수도 있다. 백만일때 v_len = 13 이었다.
+작은 데이터에 대해서는 origin이 빨랐다. value_list의 길이가 길어져야 bin_search가 빨라질 거 같은데
+value_list의 길이는 데이터가 커질 수록 안 늘어날거기 때문에... 좋은 알고리즘은 아니었다.
+"""
 from sys import stdin
 from functools import reduce
 from time import time
@@ -33,31 +39,22 @@ def insertion_sorted(lists, num):
   lists.append(num)
   return lists, i + 1
 
-# [1, 2, 4, 5] 3
-# 0 3 -> 1
-# 1 3 -> 2
-# 1 2 -> 1
-
 def binsearch(lists, num):
   low, high = 0, len(lists) - 1
-  # print(low, high)
   while(low < high):
-    mid = (low + high) // 2 # mid is index
+    mid = (low + high) // 2
     if lists[mid] > num:
       high = mid - 1
     elif lists[mid] < num:
       low = mid + 1
     else:
-      # print(num, 'happen')
       lists.insert(mid + 1, num)
       return lists, mid + 1
   
   if lists[low] > num:
-    # print('low')
     lists.insert(low, num)
     return lists , low
   else:
-    # print('low + 1')
     lists.insert(low + 1, num)
     return lists , low + 1
 
@@ -67,22 +64,19 @@ def origin_solution(N):
   value_list = [product(maketuple(num)), product(maketuple(num))]
   help_product = [[2,4], [1,4]]
   check_num = product(maketuple(2))
-  # min_v = product(value_list[0])
   while(1):
-    # print(value_list)
     if cnt == N:
       return check_num
 
     v_len = len(value_list)
     idx = 0
 
-    min_v = value_list[idx] # value_list 에서 최소값
+    min_v = value_list[idx]
     for i in range(1, v_len):
       temp = value_list[i]
       if temp < min_v:
         min_v = temp
         idx = i
-    # print(value_list, idx, sep = '\n')
     
     if check_num == min_v:
       value_list[idx] = help_product[idx][1] * value_list[idx] // help_product[idx][0]
@@ -95,7 +89,6 @@ def origin_solution(N):
       continue
 
     check_num = min_v
-    # print(idx , help_product[idx])
     value_list[idx] = help_product[idx][1] * value_list[idx] // help_product[idx][0]
     help_product[idx][0] += 1
     help_product[idx][1] += 1
@@ -111,11 +104,10 @@ def origin_solution(N):
 def new_solution_with_insertion(N):
   cnt, num = 1 , 3
   value_list = [6, 6]
+  v_len = 2
   help_product = [(2,4), (1,4)]
   check_num = product(maketuple(2))
-  # min_v = product(value_list[0])
   while(1):
-    # print(value_list)
     if cnt == N:
       return check_num
 
@@ -139,9 +131,9 @@ def new_solution_with_insertion(N):
     value_list, idx = insertion_sorted(value_list, value)
     help_product.insert(idx, (helper[0] + 1, helper[1] + 1))
 
-    v_len = len(value_list)
     if (helper[1] - helper[0]) == v_len + 1:
       num += 1
+      v_len += 1
       value = product(maketuple(num))
       value_list, idx = reverse_insertion_sorted(value_list, value)
       help_product.insert(idx, (1, num + 1))
@@ -152,29 +144,19 @@ def new_solution_with_insertion(N):
 def new_solution_with_binsearch(N): # value_list 의 길이가 그렇게 길지 않아서 bin_search가 힘을 못 쓰는 듯..
   cnt, num = 1 , 3
   value_list = [6, 6]
+  v_len = 2
   help_product = [(2,4), (1,4)]
   check_num = product(maketuple(2))
   while(1):
-    # print(value_list)
-    # if value_list == [720, 720, 756, 840]:
-    #   print(helper)
-    #   break
     if cnt == N:
       return check_num
 
     idx = 0
-    
     min_v = value_list[idx]
     helper = help_product[idx][:]
     del value_list[idx]
     del help_product[idx]
-    
-    # print(helper)
-    # min_v = value_list[idx]
-    # helper = help_product[idx][:]
-    # del value_list[idx]
-    # del help_product[idx]
-    
+ 
     if check_num == min_v:
       value = helper[1] * min_v // helper[0]
       value_list, idx = binsearch(value_list, value)
@@ -186,9 +168,9 @@ def new_solution_with_binsearch(N): # value_list 의 길이가 그렇게 길지 
     value_list, idx = binsearch(value_list, value)
     help_product.insert(idx, (helper[0] + 1, helper[1] + 1))
 
-    v_len = len(value_list)
     if (helper[1] - helper[0]) == v_len + 1:
       num += 1
+      v_len += 1
       value = product(maketuple(num))
       value_list, idx = reverse_insertion_sorted(value_list, value)
       help_product.insert(idx, (1, num + 1))
