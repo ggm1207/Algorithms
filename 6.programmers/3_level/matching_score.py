b792db47e7c2cd7561940f0b14411225d4df0bed
Gunmo <ticonhi@naver.com>
2019-11-12 21:07:57 +0900

    Update matching_score.py

--- 6.programmers/3_level/matching_score.py
+++ 6.programmers/3_level/matching_score.py
@@ -2,6 +2,21 @@
 입출력 너무 길다.. pass
 """
 
+class parser():
+  def __init__(self, txt):
+    self.txt = iter(txt)
+
+  def readline(self):
+    line = ''
+    try:
+      while(True):
+        b = next(self.txt)
+        if b == '\n':
+          return line
+        line += b
+    except StopIteration:
+      return line
+
 def solution(word, pages):
   answer = 0
   return answer
@@ -10,6 +25,14 @@ if __name__ == "__main__":
   word = "blind"
   pages = ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]
 
-  print(pages)
-  print(pages[0].findall("ko"))
-  print(pages[0][12:13])
+  f = parser(pages[0])
+
+  for i in range(20):
+    line = f.readline()
+    if line == '</body>':
+      print('body end')
+    if line == ''
+
+    if line == '':
+      break
+    print(line)
