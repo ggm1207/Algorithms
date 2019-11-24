892c341d886188ff3d057e78c43fb643214ad23a
gunmo <ggm1207@github.com>
2019-11-24 18:53:55 +0900

    gg

--- 8.hackerrank/winter1.py
+++ 8.hackerrank/winter1.py
@@ -1,9 +1,12 @@
+""" 간단한 추천 시스템 """
+
 from sys import stdin, stdout
 from collections import defaultdict
 import math
 
 if __name__ == "__main__":
 
+    # helper
     user_item_rating = defaultdict(float) # (user, item) = rating
     total_itemlist = set()                # { total item index }
     total_userlist = set()                # { total user index }
@@ -26,18 +29,14 @@ if __name__ == "__main__":
     reco_users = [int(stdin.readline().rstrip('\n')) for _ in range(num_reco_users)]
     #### input end
 
-    # print(num_sim_user_topk)    # rating 예측 시 사용할 유사 유저수
-    # print(num_item_rec_topk)    # 각 유저별로 추천해줘야 하는 아이템 개수
-    # print(num_users)            # 데이터에 있는 유저 수
-    # print(num_items)            # 데이터에 있는 아이템 수
-    # print(total_itemlist)       # 총 아이템 리스트
-    # print(num_rows)             # 
-    # print(user_item_rating)     # 유저, 아이템, 레이팅 dict
-    # print(num_reco_users)       # 
-    # print(reco_users)           # 추천결과를 만들어야 할 유저 ID
-
     avg_dict = defaultdict(float) # cache(maxsize=None)
     def avg(x): # check
+        """ 유저가 평가한 아이템들의 평균값을 반환합니다.\n
+        Args
+            x : int, user index
+        Returns
+            avg_dict[x] : float, average rating of user x for all the items rated by x
+        """
         if avg_dict[x]:
             return avg_dict[x]
         avg = 0
@@ -48,24 +47,45 @@ if __name__ == "__main__":
             
     simil_dict = defaultdict(float) # cache(maxsize=None)
     def simil(x, y): # check
+        """ 유저 x와 y의 유사도를 계산합니다. (Pearson correlation)\n
+        Args
+            x : int, user index
+            y : int, user index
+        Returns
+            simil_dict[x,y] : float, The Pearson correlation similarity of two users x, y 
+        """
         if simil_dict[x,y]: # (x, y) in cache
             return simil_dict[x,y]
-        ist_itemlist = user_itemlist[x].intersection(user_itemlist[y])
+        ist_itemlist = user_itemlist[x].intersection(user_itemlist[y]) # intersecion of item list
         rx_, ry_ = avg(x), avg(y)
-        l_d = math.sqrt(sum(map(lambda item: pow(user_item_rating[(x,item)] - rx_, 2), ist_itemlist)))
-        r_d = math.sqrt(sum(map(lambda item: pow(user_item_rating[(y,item)] - ry_, 2), ist_itemlist)))
-        up = sum(map(lambda item: (user_item_rating[(x,item)] - rx_)*(user_item_rating[(y,item)] - ry_), ist_itemlist))
-        if (l_d * r_d) == 0 or len(ist_itemlist) == 0: # Pearson correlation이 정의되지 않는 경우
+        l_d = math.sqrt(sum(map(lambda item: pow(user_item_rating[(x,item)] - rx_, 2), ist_itemlist))) # left down
+        r_d = math.sqrt(sum(map(lambda item: pow(user_item_rating[(y,item)] - ry_, 2), ist_itemlist))) # right down
+        up = sum(map(lambda item: (user_item_rating[(x,item)] - rx_)*(user_item_rating[(y,item)] - ry_), ist_itemlist)) # up
+        if (l_d * r_d) == 0 or len(ist_itemlist) == 0: # Pearson correlation이 정의되지 않는 경우 #* (분모가 0이거나 겹치는 아이템이 없을 경우)
             return 0
-        simil_dict[x,y] = up / (l_d * r_d) # cache
-        simil_dict[y,x] = simil_dict[x,y] # cache
+        simil_dict[x,y] = up / (l_d * r_d) # cache(x,y)
+        simil_dict[y,x] = simil_dict[x,y] # cache(y,x)
         return simil_dict[x,y]
 
     def get_setU(reco_user):
+        """ reco_user와 취향이 가장 유사한 num_sim_user_topk 명을 리턴합니다.\n
+        Args:
+            reco_user : int, user index
+        Returns:
+            U_ : (user, simil(reco_user, user))들을 원소로 가지며 num_sim_user_topk명 까지 유사도가 높은 순으로 정렬된 리스트입니다
+        """
         reco_user_simillist = [(user, simil(reco_user, user)) for user in total_userlist if user != reco_user]
-        return sorted(reco_user_simillist, key = lambda x: x[1], reverse=True)[:num_sim_user_topk]
+        return sorted(reco_user_simillist, key = lambda x: x[1], reverse=True)[:num_sim_user_topk] # U_
 
     def get_delU(U_, reco_user, item):
+        """ U_ 에서 item i에 대해 rating 한 유저 리스트를 반환합니다.\n
+        Args:
+            U_ : user set from get_setU()
+            reco_user : int, user index
+            item : int, item index
+        Returns:
+            D_ : U_에 있는 유저들 중에서 item i 에 대해 rating 한 유저 리스트입니다.
+        """
         reco_user_simillist = []
         for sim_user, _ in U_:
             if (sim_user, item) not in user_item_rating:
@@ -75,19 +95,19 @@ if __name__ == "__main__":
 
     ### solve
     for reco_user in reco_users:
-        reco_ratinglist = [] # 1
-        reco_items = list(total_itemlist.difference(user_itemlist[reco_user]))
+        reco_ratinglist = [] # 
+        reco_items = list(total_itemlist.difference(user_itemlist[reco_user])) # reco_user가 추천 받을 수 있는 아이템들
         U_ = get_setU(reco_user)
         
         for reco_item in reco_items:
             D_ = get_delU(U_, reco_user, reco_item)
             r_ = avg(reco_user) # 밖으로 빼내는게 좋습니다..
             sim = sum([abs(simil(reco_user, sim_user)) for sim_user in D_])
-            k = (1 / sim) if sim else 0
+            k = (1 / sim) if sim else 0 # k 값이 정의 되지 않는 경우 #* sim 값이 0일 때
             last = sum([simil(reco_user, sim_user)*(user_item_rating[(sim_user, reco_item)]  - avg(sim_user)) for sim_user in D_])
-            rui = r_ +  k * last
+            rui = r_ +  k * last # reco_user의 reco_item 에 대한 예상 rating
             reco_ratinglist.append(rui)
 
-        _, reco_items =  zip(*sorted(zip(reco_ratinglist, reco_items), reverse=True))
-        stdout.writelines(" ".join(map(str,reco_items[:num_item_rec_topk]))+'\n')
+        _, reco_items =  zip(*sorted(zip(reco_ratinglist, reco_items), reverse=True)) # 예상 rating이 높은 순으로 정렬
+        stdout.writelines(" ".join(map(str,reco_items[:num_item_rec_topk]))+'\n') # num_item_rec_topk개 만큼의 item 출력
         
--- 8.hackerrank/winter2.py
+++ 8.hackerrank/winter2.py
@@ -3,61 +3,61 @@ from collections import defaultdict
 import math
 
 def beta_var(alpha, beta):
-    # return (math.gamma(alpha)*math.gamma(beta))/math.gamma(alpha + beta)
     return alpha*beta / (pow((alpha + beta),2) * (alpha + beta + 1))
 
 if __name__ == "__main__":
 
-    T = int(stdin.readline())
-    result = []
+    T = int(stdin.readline())       # Test Case
+    result = []                     # unused
     
-    beta_init = beta_var(1.0, 15.0)
+    beta_init = beta_var(1.0, 15.0) # unused
     for testcase in range(T):
 
-        N = int(stdin.readline())
-        Sk = []
-        alpha, beta = 1.0, 15.0 # alpha 가 크면 1 beta가 크면 0
-        slot_machine = dict() # 슬롯머신의 보상 확률
-        visited = defaultdict(bool)
+        N = int(stdin.readline())       # 100 ~ 100000
+        Sk = []                         # k 번째 피드백까지 등장한 슬롯머신들의 분산들의 평균에 자연로그 스케일을 적용한 값들의 리스트
+        alpha, beta = 1.0, 15.0         # alpha, beta 초기 값
+        slot_machine = dict()           # slot_machine[item_id] = (alpha, beta)
+        visited = defaultdict(bool)     # slot_machine 방문했는지 안했는지 검사
         
         sm_len = 0
         for num in range(N):
-            item_id, kind, kind_count = stdin.readline().rstrip('\n').split() # item_id: 콘텐츠 id, imp: 노출, click: 클릭
+            item_id, kind, kind_count = stdin.readline().rstrip('\n').split() # 피드백
             item_id, kind_count = int(item_id), int(kind_count)
             flag = False
-            if not visited[item_id]:
+            if not visited[item_id]:    #! 처음 등장한 slot machine 일 경우
                 flag = True
                 visited[item_id] = True
                 slot_machine[item_id] = (alpha, beta)
                 sm_len += 1
 
-            if kind == 'imp':
+            if kind == 'imp':   #! 노출
                 o_alpha, o_beta = slot_machine[item_id]
-                slot_machine[item_id] = (o_alpha , o_beta + kind_count)
-                add_value = beta_var(o_alpha, o_beta + kind_count)
-            else:
+                slot_machine[item_id] = (o_alpha , o_beta + kind_count)             #*  보상확률 갱신
+                add_value = beta_var(o_alpha, o_beta + kind_count)                  #   더할 값
+            else:               #! 클릭
                 o_alpha, o_beta = slot_machine[item_id]
-                slot_machine[item_id] = (o_alpha + kind_count, o_beta - kind_count)
-                add_value = beta_var(o_alpha + kind_count, o_beta - kind_count)
+                slot_machine[item_id] = (o_alpha + kind_count, o_beta - kind_count) #*  보상확률 갱신
+                add_value = beta_var(o_alpha + kind_count, o_beta - kind_count)     #   더할 값
 
-            delete_value = 0
+            delete_value = 0                                                        #?  뺄 값 (처음 등장한 slot_machine)
             if not flag:
-                delete_value = beta_var(o_alpha, o_beta)
-            
-            if num == 0:
+                delete_value = beta_var(o_alpha, o_beta)                            #?  뺄 값 (기존에 있는 경우)
+            # 처음 등장한 slot matchine 일 경우 delete_value는 0의 값을 가진다
+
+            if num == 0: # 처음엔 무조건 더하는 경우만 존재한다.
                 S = add_value
             else:
                 S = result_S_sum - delete_value + add_value
 
-            result_S_sum = S
-            S = S / sm_len
-            S = math.log(S)
+            result_S_sum = S # k 번째 피드백까지 등장한 슬롯머신들의 분산의 합
+            S = S / sm_len # 평균
+            S = math.log(S) # 자연로그 스케일을 적용한 값
             
             Sk.append(S)
         
-        MAX_S = max(Sk)
-        MIN_S = min(Sk)
+        MAX_S = max(Sk) # S1 ~ SN 의 최댓값
+        MIN_S = min(Sk) # S1 ~ SN 의 최솟값
         for i, S in enumerate(Sk):
             if (S - MIN_S) <= (MAX_S - MIN_S) * 0.05:
-                stdout.writelines(str(i + 1) + '\n')
+                stdout.writelines(str(i + 1) + '\n') # 위 조건 만족할 시 break
                 break
